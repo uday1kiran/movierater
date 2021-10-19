0d086abd70206c6c586d4c2ced689dcd9078a90b
Your Name <you@example.com>
2021-10-18 19:26:04 -0700

    serializer and urls.py updated with the api models. test: http://localhost:8000/api/movies http://localhost:8000/api/ratings

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+
+from .views import MovieViewSet,RatingViewSet
+
 router = routers.DefaultRouter()
+router.register('movies',MovieViewSet)
+router.register('ratings',RatingViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
--- api/views.py
+++ api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Movie,Rating
 
 # Create your views here.
+from .serializers import MovieSerializer, RatingSerializer
+
+
+class MovieViewSet(viewsets):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+class RatingViewSet(viewsets):
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializer
