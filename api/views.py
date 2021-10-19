d47f98980b972fbc9797e5d1ee2d5b68cdaf6643
Your Name <you@example.com>
2021-10-18 23:04:52 -0700

    http://127.0.0.1:8000/api/movies/1/rate_movie/ post,body--form-data: stars as key value as 5

--- api/views.py
+++ api/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 from .models import Movie,Rating
 
 # Create your views here.
@@ -9,6 +12,29 @@ from .serializers import MovieSerializer, RatingSerializer
 class MovieViewSet(viewsets):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+
+    @action(detail=True,methods=['POST'])
+    def rate_movie(self,request,pk=None):
+        if 'stars' in request.data:
+            movie = Movie.objects.get(id=pk)
+            stars=request.data['stars']
+            user=request.user
+            try:
+                rating = Rating.objects.get(user=user.id,movie=movie.id)
+                rating.stars=stars
+                rating.save()
+                serializer=RatingSerializer(rating,many=False)
+                response = {'message': 'Rating updated','result':serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+            except:
+                rating=Rating.objects.create(user=user,movie=movie,stars=stars)
+                serializer = RatingSerializer(rating, many=False)
+                response = {'message': 'Rating created', 'result': serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+
+        else:
+            response = {'message': 'You need to provide stars'}
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
 class RatingViewSet(viewsets):
     queryset = Rating.objects.all()
